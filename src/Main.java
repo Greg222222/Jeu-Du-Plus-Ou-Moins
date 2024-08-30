763ee147ea5734c38cfe909699e64adc7540641e
Greg <greggire@hotmail.fr>
2024-08-30 12:48:05 +0200

    Ajout des try-catch et du nombre d'essais

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,50 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        // On randomise un nombre, que l'on va deviner
         Random number = new Random();
+        // Il est compris entre un minimum et un maximum
         int min = 0;
         int max = 1500;
         int nombreAleatoire = number.nextInt(max - min +1) + min;
+        // Si l'on veut connaître la réponse on peut utiliser :
+        // System.out.println(nombreAleatoire);
 
+
+        // On initialise le nombre que l'utilisateur va entrer (avec une valeur ne pouvant pas être le résultat)
         int guess = 1501;
+        // On initialise un comptage pour savoir combien de guess l'utilisateur a fait
+        int i = 0;
+        // On créé un Scanner
+        Scanner s = new Scanner(System.in);
+        // Boucle tant que l'utilisateur n'a pas deviné
         while (guess != nombreAleatoire) {
-
-            Scanner s = new Scanner(System.in);
-            System.out.println("Entrez votre guess");
-            guess = s.nextInt();
-
-            if (guess < nombreAleatoire) {
-                System.out.println("Vous avez demandé " + guess + " c'est plus");
+            // On vérifie que l'utilisateur a bien rentré un entier
+            try {
+                System.out.println("Entrez votre guess");
+                guess = s.nextInt();
+                // On incrémente le nombre d'essais
+                i++;
+                // Puis on vérifie le guess
+                if (guess < nombreAleatoire) {
+                    System.out.println("Vous avez proposé " + guess + " c'est plus");
+                }
+                else if (guess > nombreAleatoire ) {
+                    System.out.println("Vous avez proposé " + guess + " c'est moins");
+                }
             }
-            else if (guess > nombreAleatoire ) {
-                System.out.println("Vous avez demandé " + guess + " c'est moins");
+            // S'il n'a pas rentré un entier, on le lui fait savoir
+            catch (InputMismatchException e) {
+               System.out.println("Vous n'avez pas entré un nombre");
+               // Et on le fait recommencer
+               s.next(); // Consomme l'entrée incorrecte
             }
         }
         {
-            System.out.println("Vous avez demandé " + guess + " ET C'EST TOUT PILE");
+            System.out.println("Vous avez proposé " + guess + " ET C'EST TOUT PILE !! Réussi en " + i + " essais");
         }
 
     }
